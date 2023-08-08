8fc4b37e2eb69ad0d6c55ae3b00a87cdc7643bc7
Rahim_Badsa <rahimbadsa723@gmail.com>
2023-08-08 21:26:21 +0600

    problem 3 is done with testing

--- assignment 4.js
+++ assignment 4.js
@@ -7,7 +7,7 @@ function cubeNumber(number) {
         return cubeNumberResult;
     }
 }
-console.log(cubeNumber(3))
+// console.log(cubeNumber(3))
 
 // Problem 2 “matchFinder()”
 function matchFinder(string1, string2) {
@@ -18,3 +18,23 @@ function matchFinder(string1, string2) {
         return matchFinderResult;
     }
 }
+// console.log(matchFinder("123", "1"));
+
+//Problem 3 sortMaker()
+
+function sortMaker(arr) {
+    let reversedArray = [];
+
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i]<0){
+            return "Invalid Input"
+        }else if(arr[0] === arr[1]){
+            return "equal"
+        }else{
+            reversedArray.unshift(arr[i]);
+        }
+    }
+    return reversedArray;
+}
+// console.log(sortMaker([4,2]));
+
